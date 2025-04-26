5c0c9d825e2e2454a7d3e1cfbb02287ad64d8773
neuylduy24 <duyluyent@gmail.com>
2025-04-26 19:01:58 +0700

    first commit

---
+++ backend-api/Multiple choice test system/Multiple-choice-test-system/src/main/java/com/example/Multiple/choice/test/system/MultipleChoiceTestSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.Multiple.choice.test.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipleChoiceTestSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultipleChoiceTestSystemApplication.class, args);
+	}
+
+}
---
+++ backend-api/Multiple choice test system/Multiple-choice-test-system/src/test/java/com/example/Multiple/choice/test/system/MultipleChoiceTestSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Multiple.choice.test.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultipleChoiceTestSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
